b77388249d2c62362dddf18273bf2de0105a1ca8
thinh204 <huythinh592@gmail.com>
2024-07-06 18:30:02 +0700

    Done

---
+++ top-50-ideas/src/Pages/Users/homePage/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { memo } from "react";
+
+const HomePage = () => {
+  return <h1>HomePage</h1>;
+};
+
+export default memo(HomePage); 
--- top-50-ideas/src/main.jsx
+++ top-50-ideas/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter } from "react-router-dom";
+import HomePage from './Pages/Users/homePage';
+import RouterCustom from "./router.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <BrowserRouter>
+        <RouterCustom />
+    </BrowserRouter>
+);
